5839a2532c58bc5fc28c2eff453778f94cb506b1
rajthakkar02 <rajthakkar0302@gmail.com>
2023-10-06 15:06:57 +0530

    Updated the Main File

--- Training/src/main/java/org/example/Main.java
+++ Training/src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import org.example.sorting.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BubbleSort.bubbleSort();
+        SelectionSort.selectionSort();
+        InsertionSort.insertionSort();
+
+
+        int[] arr = {6,3,5,9,2,8};
+        int n = arr.length;
+
+        QuickSort.quickSort(arr,0,n-1);
+
+        for (int i=0 ; i< arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
     }
 }
--- Training/src/main/java/org/example/sorting/QuickSort.java
+++ Training/src/main/java/org/example/sorting/QuickSort.java
@@ -3,33 +3,39 @@ package org.example.sorting;
 public class QuickSort {
 
     public static void quickSort(int[] arr,int low,int high){
-        if (low < high){
-            int pivot = partition(arr,low,high);
-
-            quickSort(arr,low,pivot-1);
-            quickSort(arr,pivot+1,high);
+        if (low >= high){
+            return;
+          
         }
+        int pivot = partition(arr,low,high);
+
+        quickSort(arr,low,pivot-1);
+        quickSort(arr,pivot+1,high);
     }
 
     public static int partition(int[] arr,int low,int high){
-        int pivot = arr[high];
         int i = low-1;
 
         for (int j=low; j< high;j++){
-            if (arr[j] < pivot){
-                i++;
-
-                // Swap
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+            if (!(arr[j] < arr[high])){
+                continue;
             }
+            i++;
+
+            // Swap
+            swap(arr, i, j);
         }
         i++;
+
         //Swap pivot
-        int temp = arr[i];
-        arr[i] = pivot;
-        arr[high] = temp;
+        swap(arr, i, high);
+
         return i; //pivot index
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
